5ff3fad8f5a0d2cd47855ebb0e94be32c49b9438
dvip1 <dvip.d.patel@slrtce.in>
2023-08-14 20:55:09 +0530

    minor ui improvements

--- src/components/feature/cards.tsx
+++ src/components/feature/cards.tsx
@@ -2,31 +2,33 @@ import { GlobalStyles } from "../../styles/homeStyle";
 import { CardData } from "../../data/cardData";
 
 export default function Cards() {
-    return (
-        <>
-            <GlobalStyles />
-            <div className="mx-auto mt-5 bento-grid">
-                {CardData.map((card, index) => {
-                    return (
-                        <div className="bento-grid-item my-4 " key={index}>
-                            <a href="#" >
-                                <div className="bg-[#faeaf7] shadow-md rounded-lg overflow-hidden hover:bg-[#f0c1e8] transition duration-300">
-                                    <img
-                                        src={card.img}
-                                        alt={card.title}
-                                        className="w-full h-40 object-cover rounded-xl shadow-lg"
-                                    />
-                                    <div className="p-4">
-                                        <h2 className="text-xl font-medium mb-2">{card.title}</h2>
-                                        <p className="text-gray-600 info">{card.info}</p>
-                                        <div className="mt-4"></div>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    );
-                })}
-            </div>
-        </>
-    );
+  const handleCardClick = (url: string) => {
+    window.location.href = url;
+  };
+
+  return (
+    <>
+      <GlobalStyles />
+      <div className="mt-5 bento-grid">
+        {CardData.map((card, index) => (
+          <div className="bento-grid-item my-4" key={index}>
+            <button
+              className="bg-[#faeaf7] hover:bg-[#f0c1e8] transition duration-300 rounded-xl shadow-lg overflow-hidden"
+              onClick={() => handleCardClick(card.url)}
+            >
+              <img
+                src={card.img}
+                alt={card.title}
+                className="w-full h-40 object-cover shadow-lg rounded-xl"
+              />
+              <div className="p-4 text-left">
+                <h2 className="text-xl font-medium mb-2 ">{card.title}</h2>
+                <p className="text-gray-600 info">{card.info}</p>
+              </div>
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
--- src/components/feature/homeFeatures.tsx
+++ src/components/feature/homeFeatures.tsx
@@ -6,27 +6,29 @@ export function Hero() {
     return (
         <>
             <GlobalStyles />
-                <div className="grid md:grid-cols-2 gap-4 debug-screens my-container mt-10">
-                    {/* Item 1 */}
-                    <div className="md:col-span-1 md:order-1  order-2 flex justify-center items-center ">
-                        <div className="p-4 text-left">
-                            <h1 className="xl:text-6xl lg:text-5xl text-4xl font-semibold">
-                                Join in on my weird <span className="textGrad">Journeys</span>
-                            </h1>
-                            <p className="subtitle text-base">
-                                Discover my personal Blog - A collection of things that I love</p>
-                            <PrimaryButton className="rounded-md"> Subscribe  <i className="fas fa-envelope"></i></PrimaryButton>
-                            <SecondaryButton className="rounded-md"> Github  <i className="fab fa-github"></i></SecondaryButton>
-                        </div>
-                    </div>
-
-                    {/* Item 2 */}
-                    <div className="md:col-span-1 md:order-2 order-1">
-                        <div className="p-4">
-                            <img src="./src/assets/Journeys.svg" alt="" style={{ maxWidth: '100%' }} />
-                        </div>
+            <div className="grid md:grid-cols-2 gap-4 debug-screens my-container mt-10">
+                {/* Item 1 */}
+                <div className="md:col-span-1 md:order-1  order-2 flex justify-center items-center ">
+                    <div className="p-4 text-left">
+                        <h1 className="xl:text-6xl lg:text-5xl text-4xl font-semibold">
+                            Join in on my weird <span className="textGrad">Journeys</span>
+                        </h1>
+                        <p className="subtitle text-base">
+                            Discover my personal Blog - A collection of things that I love</p>
+                        <PrimaryButton className="rounded-md"> Subscribe  <i className="fas fa-envelope"></i></PrimaryButton>
+                        <SecondaryButton className="rounded-md"> Github  <i className="fab fa-github"></i></SecondaryButton>
                     </div>
                 </div>
+
+                {/* Item 2 */}
+                {/* Item 2 */}
+                <div className="md:col-span-1 md:order-2 order-1 flex justify-center items-center md:block">
+                    <div className="p-4">
+                        <img src="./src/assets/Journeys.svg" alt="" className="w-full xl:w-10/12" />
+                    </div>
+                </div>
+
+            </div>
         </>
     );
 }
