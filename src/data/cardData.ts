84984c871bcadfb70bc453798eec42ebef289b87
dvip1 <dvip.d.patel@slrtce.in>
2023-08-15 18:57:53 +0530

    articles page

--- src/data/cardData.ts
+++ src/data/cardData.ts
@@ -3,7 +3,6 @@ export type CardType = {
     title: string
     info: string
     img: string
-    url: string
 }
 export let CardData: CardType[] = [
     {
@@ -11,14 +10,12 @@ export let CardData: CardType[] = [
         title: "Web Development",
         info: "I can build beautiful and scalable web apps using HTML, CSS and React.js",
         img: "https://lh3.googleusercontent.com/51moDkmVWptBFXRReuACKocS_fsOoYtfT-C78onEUKeRr7ky4l_ZsuZMK2AnE7-MPRbLzcKTAIFmNyyN7ACo0-eygmXaGeR0ItYtRcYNE8-_L8RHqkA=w960",
-        url: "#",
     },
     {
         id: 2,
         title: "App Development",
         info: "I can build beautiful and scalable mobile apps using Flutter and Dart",
         img: "https://lh3.googleusercontent.com/aiJojq_gpPLqLhU0PZH6Dog-OjIMti_hiclF_6oqxucGuvywn_AEtu2Qrk2f4WxEhRWMjXkz8qXpTCGoj5TZ0DUCEZ5UIXnXTSIu_7ndg2dTNmQbdkCO=w960",
-        url: "#",
 
     },
     {
@@ -26,7 +23,6 @@ export let CardData: CardType[] = [
         title: "UI/UX Design",
         info: "I can design beautiful and scalable UI/UX using Figma and Adobe XD",
         img: "https://lh3.googleusercontent.com/mLGakWFjdX3zGBIhCUWqzjlj3gUXOY8hOed_ISzomlpNmjKWUiqCFyNGGAZFneVaK30v4GUpjn7lyIsI2Z7KPvMKrYkumzBr2nwbVe3BQKRkpam3sA=w960",
-        url: "#",
 
     },
 
