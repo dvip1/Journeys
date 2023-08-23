4aca9c06a3932791793266d1414ad66f5dc46227
dvip1 <dvip.d.patel@slrtce.in>
2023-08-23 21:54:22 +0530

    improving code ergonomics

--- src/pages/articles.tsx
+++ src/pages/articles.tsx
@@ -1,14 +1,17 @@
 import { useParams, Params } from "react-router-dom";
 import { CardData } from "../data/cardData";
-import Code from "../components/feature/code";
-import Paragraph from "../components/feature/text";
+import ArticleHeader from "../components/UI/ArticleHeader";
+import ArticleContent from "../components/feature/ArticleContent";
+import RelatedArticles from "../components/feature/RelatedArticles";
+
 interface ArticleParams extends Params {
   articleId: string;
 }
+
 export default function Articles() {
   const { articleId = "" } = useParams<ArticleParams>();
-  const article = CardData.find((card) => card.id === parseInt(articleId));
 
+  const article = CardData.find((card) => card.id === parseInt(articleId));
   if (!article) {
     return <div>Article not found</div>;
   }
@@ -17,34 +20,11 @@ export default function Articles() {
     <div className="container mx-auto mt-10">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
         <div className="md:col-span-2">
-          <img
-            src={article.img}
-            alt={article.title}
-            className="w-full h-96 object-cover lg:rounded-lg shadow-lg"
-          />
-          <h1 className="text-3xl font-bold mt-5">{article.title}</h1>
-          <p className="text-gray-600 mt-2">{article.info}</p>
-          <div className="mt-5 prose">
-            {article.content.map((component) => {
-              switch (component.component) {
-                case "text":
-                  return <p key={component.id}>{component.text}</p>;
-                case "code":
-                  return <Code key={component.id} code={component.text} />;
-                case 'paragraph':
-                  return <Paragraph key={component.id} text={component.text} />;
-
-                default:
-                  return null;
-              }
-            })}
-          </div>
+          <ArticleHeader article={article} />
+          <ArticleContent article={article} />
         </div>
         <div className="md:col-span-1">
-          <h2 className="text-xl font-bold mb-5">Related Articles</h2>
-          <div className="grid grid-cols-1 gap-5">
-            {/* Add related articles here */}
-          </div>
+          <RelatedArticles />
         </div>
       </div>
     </div>
