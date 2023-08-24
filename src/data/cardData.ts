b3e3c643ea5f8a559cf22dc9bb6eb9a2ad0bdbb6
dvip1 <dvip.d.patel@slrtce.in>
2023-08-24 18:32:21 +0530

    improving articles page i

--- src/data/cardData.ts
+++ src/data/cardData.ts
@@ -10,6 +10,7 @@ export type CardType = {
         code?: string
         image?: string
         paragraph?: string
+        isDropCap?: boolean
     }[]
 }
 export let CardData: CardType[] = [
@@ -19,11 +20,23 @@ export let CardData: CardType[] = [
         info: "I can build beautiful and scalable web apps using HTML, CSS and React.js",
         img: "https://lh3.googleusercontent.com/51moDkmVWptBFXRReuACKocS_fsOoYtfT-C78onEUKeRr7ky4l_ZsuZMK2AnE7-MPRbLzcKTAIFmNyyN7ACo0-eygmXaGeR0ItYtRcYNE8-_L8RHqkA=w960",
         content: [
-            { id: 1, component: 'text', text: 'Today I want to share my experience with React.' },
-            { id: 2, component: 'code', text: 'function helloWorld(){return "Hello World"}' },
             {
-                id: 3, component: "paragraph", text: "Though my own family did not make miso at home, it was indispensable to us nonetheless, as it was (and still is) in all Japanese kitchens. Misoshiru, or miso soup, is served almost every day—either with rice and pickled vegetables as a complete (if frugal) meal, or on its own as the standard breakfast. I use miso as a base for all kinds of sauces and dressings, and like many people, I believe it to be essential when braising or grilling fish, especially strong-flavored mackerel. And I wouldn't cook beef without it.     "   }
-            
+                id: 1,
+                component: "paragraph",
+                text:
+                  "Though my own family did not make miso at home, it was indispensable to us nonetheless, as it was (and still is) in all Japanese kitchens. Misoshiru, or miso soup, is served almost every day—either with rice and pickled vegetables as a complete (if frugal) meal, or on its own as the standard breakfast. I use miso as a base for all kinds of sauces and dressings, and like many people, I believe it to be essential when braising or grilling fish, especially strong-flavored mackerel. And I wouldn't cook beef without it.",
+                isDropCap: true,
+              },
+              {
+                id: 2,
+                component: "paragraph",
+                text:
+                  "I decided that I needed to try on The Bra. Full disclosure: there is no undergarment in the world that would gird my loins enough to prepare me for combat. I shy away from quarrels; I am afraid of bear spray. Clothes and gear, however, are another story, and, surprisingly, we owe many of the things that we wear and use every day to the military: beanies, cargo pants, T-shirts, trenchcoats, and aviator glasses—and can we agree that sanitary napkins count as gear? Duct tape, Cheetos, and Silly Putty all have military origins. And, of course, the bra.",
+                isDropCap: false,
+              },
+
+
+
         ]
 
     },
--- src/pages/articles.tsx
+++ src/pages/articles.tsx
@@ -2,7 +2,6 @@ import { useParams, Params } from "react-router-dom";
 import { CardData } from "../data/cardData";
 import ArticleHeader from "../components/UI/ArticleHeader";
 import ArticleContent from "../components/feature/ArticleContent";
-import RelatedArticles from "../components/feature/RelatedArticles";
 
 interface ArticleParams extends Params {
   articleId: string;
@@ -17,14 +16,11 @@ export default function Articles() {
   }
 
   return (
-    <div className="container mx-auto mt-10">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
+    <div className="mx-auto sm:mt-10 container">
+      <div className="grid grid-cols-1  gap-10">
         <div className="md:col-span-2">
           <ArticleHeader article={article} />
           <ArticleContent article={article} />
-        </div>
-        <div className="md:col-span-1">
-          <RelatedArticles />
         </div>
       </div>
     </div>
