dfc1511885b1261a793c322a65642cb2fef4e318
dvip1 <dvip.d.patel@slrtce.in>
2023-08-25 18:21:36 +0530

    seprating components

---
+++ src/components/UI/ArticleHeader.tsx
@@ -0,0 +1,21 @@
+import { CardType } from "../../data/cardData";
+
+interface ArticleHeaderProps {
+    article: CardType;
+}
+
+export default function ArticleHeader({ article }: ArticleHeaderProps) {
+    return (
+        <div className="flex flex-col md:flex-row md:items-center">
+            <img
+                src={article.img}
+                alt={article.title}
+                className="w-full md:w-1/2 h-auto md:h-96 object-cover lg:rounded-lg shadow-lg mb-5 md:mb-0 md:mr-5 "
+            />
+            <div className="md:w-1/2">
+                <h1 className="text-3xl font-bold mt-5">{article.title}</h1>
+                <p className="text-gray-600 mt-2">{article.info}</p>
+            </div>
+        </div>
+    );
+}
